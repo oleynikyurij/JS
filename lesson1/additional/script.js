485b8af52cb1e1d78f630614c76f5227782a2ad4
unknown <D:\\Mail>
2018-08-08 23:24:26 +0300

    replace Math.pow()

--- lesson1/additional/script.js
+++ lesson1/additional/script.js
@@ -3,7 +3,9 @@ var num = "33721";
 var composition = +num[0] * +num[1] * +num[2] * +num[3] * +num[4];
 
 
-var stepen = Math.pow(composition, 3);
+// var stepen = Math.pow(composition, 3);
+
+var stepen = composition ** 3;
 
 var resultTwo = (""+stepen).slice(0, 2);
 
