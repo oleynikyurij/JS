98db179646355353fa5e83335b220222c07cbe3f
unknown <D:\\Mail>
2018-08-21 19:53:46 +0300

    basic and additional

--- lesson_8/additional/js/script.js
+++ lesson_8/additional/js/script.js
@@ -1,28 +1,30 @@
 let nav = document.querySelector('.nav'),
 	link = document.getElementsByTagName('a');
 
+	//кнопка на верх
 	let up = document.querySelector('.top');
-		
 			up.onclick = ()=> window.scrollTo(0, 0);
 			
 
-
+//обработчик клика меню
 nav.addEventListener('click', (e) => {
 	e.preventDefault();
 	for (let i = 0; i < link.length; i++) {
 		if (e.target === link[i]) {
 			// console.log(link[i]);
 			// console.log(e.target.hash);
-
+//получаем div соответствующий пункту меню
 			let divId = document.getElementById((e.target.hash).slice(1));
 			// console.log(divId);
 
+			//положение div
 			let h = divId.offsetTop,
 			curH = window.pageYOffset;
 			console.log(h, curH);
 
 			scrollDiv();
 
+			//функция плавного скролла к заданному div 
 			function scrollDiv() {
 
 				let scroll = setInterval(function () {
