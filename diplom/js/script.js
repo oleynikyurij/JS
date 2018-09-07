838212d3ff6fceda45c9931934bc6b60f98381e8
unknown <D:\\Mail>
2018-09-07 15:06:40 +0300

    Форма в модальном окне

--- diplom/js/script.js
+++ diplom/js/script.js
@@ -6,6 +6,7 @@ import tarif from './parts/tarif';
 import transitionPage from './parts/transition_page';
 import smoothScrolling from './parts/smooth_scrolling';
 import authorizationForm from './parts/authorization_form';
+import formModal from './parts/form_modal';
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -17,6 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	transitionPage();
 	smoothScrolling();
 	authorizationForm();
+	formModal();
 	
 	
 
